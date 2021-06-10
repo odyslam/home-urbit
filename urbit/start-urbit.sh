84d9623efc191668f39f0733bf9d3e1c42d3eefe
odyslam <odyslam@gmail.com>
2021-06-10 20:21:09 +0300

    fix start-urbit.sh flow for new users

--- urbit/start-urbit.sh
+++ urbit/start-urbit.sh
@@ -12,27 +12,19 @@ echo "Starting Urbit with Ames port: $AMES_PORT"
 if [ -z "$dirname" ]; then
     if [ -e *.key ]; then
         echo "Urbit detected a .key file"
+        echo "Urbit will use $keyname.key to create a pier and then restart"
         # Get the name of the key
         keynames="*.key"
         keys=( $keynames )
         keyname=''${keys[0]}
         mv $keyname /tmp
         # Boot urbit with the key, exit when done booting
-        urbit -t -p $AMES_PORT -w $(basename $keyname .key) -k /tmp/$keyname -c $(basename $keyname .key) 
+        urbit -x -t -p $AMES_PORT -w $(basename $keyname .key) -k /tmp/$keyname -c $(basename $keyname .key) 
         # Remove the keyfile for security
+        echo "Urbit created a pier from the key $keyname"
         rm /tmp/$keyname
         rm *.key || true
-    elif [ -e *.comet ]; then
-        echo "Urbit detected a .comet file"
-        cometnames="*.comet"
-        comets=( $cometnames )
-        cometname=''${comets[0]}
-        rm *.comet3
-        urbit -t -p $AMES_PORT  -c $(basename $cometname .comet) 
-
-    elif [ -z "$COMET_NAME" ]; then
-        echo "Urbit detected the env variable COMET_NAME set to: $COMET_NAME"
-        urbit -t -p $AMES_PORT -c "$COMET_NAME"
+        echo "Urbit will now restart the container"
     else 
         random=$RANDOM
         echo "Urbit did not detect any user selection. Booting a comet with the random name: comet-$random."
