e379c543601a13a0ab0ba35b54f395180aadd1f0
odyslam <odyslam@gmail.com>
2021-06-12 20:20:58 +0300

    chnang defualt passwrd

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 if [[ -z "${NGINX_USER}" ]]; then
-    export NGINX_USER='urbit-home'
+    export NGINX_USER='home-urbit'
     echo "NGINX_USER is set to default value: $NGINX_USER"
 fi
 if [[ -z "${NGINX_PASSWORD}" ]]; then
-    export NGINX_PASSWORD='urbit-home'
+    export NGINX_PASSWORD='home-urbit'
     echo "NGINX_PASSWORD is set to default value: $NGINX_PASSWORD"
 fi
   
